f8f42f28c1fdac0eec57cdde31fd401acba6185b
Resul42 <Resul-g_93@hotmail.com>
2014-12-18 17:01:36 +0100

    First Commit

--- angularscript.js
+++ angularscript.js
@@ -1,17 +1,15 @@
 
-
-
 angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope, $http) {
 	$scope.selected = "";
-    $http.get("Processor.json").then(function(response){
+    $http.get("JSON/Processor.json").then(function(response){
  		$scope.componentsCPU = response.data;
     });
 
 
 // Any function returning a promise object can be used to load values asynchronously
     $scope.gpuselected = "";
-    $http.get("Graphicscard.json").then(function(response){
+    $http.get("JSON/Graphicscard.json").then(function(response){
         $scope.componentsGPU = response.data;
     });
 
