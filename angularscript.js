b05127ee352bb6939d2c33ada593366f8e80d3db
Resul42 <Resul-g_93@hotmail.com>
2014-12-18 17:36:42 +0100

    Saved Input

--- angularscript.js
+++ angularscript.js
@@ -5,7 +5,10 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
     $http.get("JSON/Processor.json").then(function(response){
  		$scope.componentsCPU = response.data;
     });
-
+    $scope.master = {};
+    $scope.update = function(selected) {
+        $scope.master = angular.copy(selected);
+    };
 
 // Any function returning a promise object can be used to load values asynchronously
     $scope.gpuselected = "";
@@ -14,6 +17,5 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
     });
 
 });
-
 
 
