1708c4c415a9064e51ec24378fe9b3082fc2fcd3
Resul42 <Resul-g_93@hotmail.com>
2015-01-09 13:41:32 +0100

    f

--- angularscript.js
+++ angularscript.js
@@ -2,19 +2,25 @@
 angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope, $http) {
 	$scope.selected = "";
-    $http.get("JSON/Processor.json").then(function(response){
+    $http.get("JSON/mongodb-dumpCPU.json").then(function(response){
  		$scope.componentsCPU = response.data;
     });
-    $scope.master = {};
-    $scope.update = function(selected) {
-        $scope.master = angular.copy(selected);
-    };
 
 // Any function returning a promise object can be used to load values asynchronously
     $scope.gpuselected = "";
-    $http.get("JSON/Graphicscard.json").then(function(response){
+    $http.get("JSON/mongodb-dumpGPU2.json").then(function(response){
         $scope.componentsGPU = response.data;
     });
+
+    $scope.memoryselected = "";
+    $http.get("JSON/mongodb-dumpMEMORY.json").then(function(response){
+        $scope.componentsMemory = response.data;
+    });
+
+    $scope.computer = {};
+    $scope.update = function(selected) {
+        $scope.computer = angular.copy(selected);
+    };
 
 });
 
