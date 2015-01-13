44ac5920f1ec49173f839b93444a0a5666d2dbaf
Resul42 <Resul-g_93@hotmail.com>
2015-01-13 15:20:29 +0100

    Moederboard toegevoegd

--- angularscript.js
+++ angularscript.js
@@ -2,19 +2,34 @@
 angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope, $http) {
 	$scope.selected = "";
-    $http.get("JSON/Processor.json").then(function(response){
+    $http.get("JSON/PROCESSOR.json").then(function(response){
  		$scope.componentsCPU = response.data;
     });
-    $scope.master = {};
-    $scope.update = function(selected) {
-        $scope.master = angular.copy(selected);
-    };
 
 // Any function returning a promise object can be used to load values asynchronously
     $scope.gpuselected = "";
-    $http.get("JSON/Graphicscard.json").then(function(response){
+    $http.get("JSON/GRAPHICSCARD.json").then(function(response){
         $scope.componentsGPU = response.data;
     });
+
+    $scope.memoryselected = "";
+    $http.get("JSON/MEMORY.json").then(function(response){
+        $scope.componentsMemory = response.data;
+    });
+
+    $scope.motherboardselected = "";
+    $http.get("JSON/MOTHERBOARD.json").then(function(response){
+        $scope.componentsMotherboard = response.data;
+    });
+
+    $scope.memoryselected = "";
+    $http.get("JSON/OPTICALDRIVE.json").then(function(response){
+        $scope.componentsOD = response.data;
+    });
+    $scope.computer = {};
+    $scope.update = function(selected) {
+        $scope.computer = angular.copy(selected);
+    };
 
 });
 
