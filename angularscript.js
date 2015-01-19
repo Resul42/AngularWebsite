8e3529cf815d4ec78e0e72fb4937b40f94e98ab2
Resul42 <Resul-g_93@hotmail.com>
2015-01-19 16:07:23 +0100

    Jsons added

--- angularscript.js
+++ angularscript.js
@@ -5,27 +5,37 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
     $http.get("JSON/PROCESSOR.json").then(function(response){
  		$scope.componentsCPU = response.data;
     });
-
 // Any function returning a promise object can be used to load values asynchronously
-    $scope.gpuselected = "";
+    $scope.selectedGPU = "";
     $http.get("JSON/GRAPHICSCARD.json").then(function(response){
         $scope.componentsGPU = response.data;
     });
 
-    $scope.memoryselected = "";
+    $scope.selectedMemory = "";
     $http.get("JSON/MEMORY.json").then(function(response){
         $scope.componentsMemory = response.data;
     });
 
-    $scope.motherboardselected = "";
+    $scope.selectedMotherboard = "";
     $http.get("JSON/MOTHERBOARD.json").then(function(response){
         $scope.componentsMotherboard = response.data;
     });
 
-    $scope.memoryselected = "";
+    $scope.selectedMemory = "";
     $http.get("JSON/OPTICALDRIVE.json").then(function(response){
         $scope.componentsOD = response.data;
     });
+
+    $scope.selectedPowersupply = "";
+    $http.get("JSON/PSU.json").then(function(response){
+        $scope.componentsPSU = response.data;
+    });
+
+    $scope.selectedPowersupply = "";
+    $http.get("JSON/CASE.json").then(function(response){
+        $scope.componentsCASE = response.data;
+    });
+
     $scope.computer = {};
     $scope.update = function(selected) {
         $scope.computer = angular.copy(selected);
